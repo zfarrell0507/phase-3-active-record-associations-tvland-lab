ab1be45dd17a29de251792e930a1ae88e1e5d4d4
Zachary Farrell <zfarrell0507@gmail.com>
2022-09-13 19:58:10 -0400

    commit tree 7e6cc35ef97a3ad3d6b4adf8e7c0d16f9254a928

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,9 @@
 class Network < ActiveRecord::Base
-  
+    has_many :shows
+
+    def sorry
+        if self.call_letters == "NBC"
+            "We're sorry about passing on John Mulaney's pilot"
+        end
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        self.characters.map do |character|
+            "#{character.actor.first_name} #{character.actor.last_name}"
+        end
+    end
 end
